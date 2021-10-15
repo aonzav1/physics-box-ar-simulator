7e6ee8ed0cef2d78f071b5ed0c9c96d8af5637b7
banksinn <sintanawongsupakdee@gmail.com>
2021-10-15 19:42:05 +0700

    create rbClick

---
+++ AB Mechanic simulation/Assets/Scripts/Physics/testcube.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testcube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rbClick();
+        }
+    }
+    public void rbClick()
+    {
+        rb.AddForce(new Vector3(0, 100, 0));
+    }
+}
