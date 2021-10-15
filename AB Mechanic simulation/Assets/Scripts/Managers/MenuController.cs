5cf84865542fbeb05d41fa483a9785291046afe0
aonzav1 <aon3975@hotmail.co.th>
2021-10-16 01:14:47 +0700

    Add Simple UI Asset
    - Add new UI Asset
    - Decorate new main menu UI

--- AB Mechanic simulation/Assets/Scripts/Managers/MenuController.cs
+++ AB Mechanic simulation/Assets/Scripts/Managers/MenuController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject mainMenuPage;
+    public GameObject modeSelectionPage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OpenPage(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OpenPage(int num)
+    {
+        mainMenuPage.SetActive(num == 0);
+        modeSelectionPage.SetActive(num == 1);
     }
 }
